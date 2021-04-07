24d989f200dfbf95f904329e47237c2780584b4a
David Niklas Adamson <david.n.adamson@stud.h-da.de>
2021-04-07 23:49:16 +0200

    feat: serializing uuid of animationLayer

--- ledwall-rest-plugin/src/main/java/io/aelite/ledwall/restplugin/dto/AnimationLayerDTO.java
+++ ledwall-rest-plugin/src/main/java/io/aelite/ledwall/restplugin/dto/AnimationLayerDTO.java
@@ -4,15 +4,22 @@ import io.aelite.ledwall.core.animation.control.Control;
 import io.aelite.ledwall.core.animation.layer.AnimationLayer;
 
 import java.util.Map;
+import java.util.UUID;
 
 public class AnimationLayerDTO {
 
+    private UUID uuid;
     private String name;
     private Map<String, Control> controls;
 
     public AnimationLayerDTO(AnimationLayer animationLayer){
+        this.uuid = animationLayer.getUuid();
         this.name = animationLayer.getName();
         this.controls = animationLayer.getControls();
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 
     public String getName() {
