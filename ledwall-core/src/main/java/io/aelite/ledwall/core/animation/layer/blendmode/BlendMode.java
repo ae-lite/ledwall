ac4f0d01a6bdee53f7ec64765b6b04e5c09b0410
David Niklas Adamson <david.n.adamson@stud.h-da.de>
2021-04-08 21:24:35 +0200

    fix: remove serialization annotation from blend mode interface

--- ledwall-core/src/main/java/io/aelite/ledwall/core/animation/layer/blendmode/BlendMode.java
+++ ledwall-core/src/main/java/io/aelite/ledwall/core/animation/layer/blendmode/BlendMode.java
@@ -1,8 +1,5 @@
 package io.aelite.ledwall.core.animation.layer.blendmode;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public interface BlendMode {
 
     public static BlendMode NORMAL = new NormalBlendMode();
